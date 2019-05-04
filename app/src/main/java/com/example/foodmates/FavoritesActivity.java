9c73443243dbede148adec9c1ae452df05fc21a4
Serena Dose <serena.dose@gmail.com>
2019-05-04 14:18:38 +0200

    feat: botto navigationg view

--- app/src/main/java/com/example/foodmates/FavoritesActivity.java
+++ app/src/main/java/com/example/foodmates/FavoritesActivity.java
@@ -1,7 +1,9 @@
 package com.example.foodmates;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,6 +12,8 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 //Test
 
@@ -18,6 +22,7 @@ public class FavoritesActivity extends AppCompatActivity {
     ViewPager viewPager;
     PageAdapter pageAdapter;
     TabLayout tabLayout;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,34 @@ public class FavoritesActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs_favorites);
         tabLayout.setupWithViewPager(viewPager);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_bar_favorites);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //Qui in realtà verrà aperta una nuova activity con gli intent
+
+                switch (menuItem.getItemId()){
+                    case R.id.action_home:
+                        Toast.makeText(getApplicationContext(), "Favorite", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_search:
+                        Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_add:
+                        Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_favorites:
+                        Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_profile:
+                        Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
 
         Typeface typeface = ResourcesCompat.getFont(FavoritesActivity.this, R.font.nunito_regular);
     }
