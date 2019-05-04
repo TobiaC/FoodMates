f7a0367d3678d361b5ea5c6c99ff6f735259aeb8
Serena Dose <serena.dose@gmail.com>
2019-05-04 19:21:57 +0200

    test branch

--- app/src/main/java/com/example/foodmates/CanaliFavoritesFragment.java
+++ app/src/main/java/com/example/foodmates/CanaliFavoritesFragment.java
@@ -16,6 +16,7 @@ public class CanaliFavoritesFragment extends Fragment {
 
         //Qui si fa riferimento all'xml
         return inflater.inflate(R.layout.fragment_canali_favorites, container,false);
+
         
     }
 
