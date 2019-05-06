ca87bdd7e5437e12577df008b501bb97a31dcbd9
Serena Dose <serena.dose@gmail.com>
2019-05-06 22:37:03 +0200

    Fix: Bottom navigation view finally works with fragments!

---
+++ app/src/main/java/com/example/foodmates/Fragment_home.java
@@ -0,0 +1,28 @@
+package com.example.foodmates;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment_home extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View result = inflater.inflate(R.layout.fragment_home, container,false);
+        return result;
+    }
+
+}
--- app/src/main/java/com/example/foodmates/MainActivity.java
+++ app/src/main/java/com/example/foodmates/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,7 +13,12 @@ import android.widget.Toast;
 //Test
 
 public class MainActivity extends AppCompatActivity {
-    
+
+     Fragment fragmentHome = new Fragment_home();
+     Fragment fragmentFav = new Fragment_favorites();
+    FragmentManager fm = getSupportFragmentManager();
+
+    Fragment active = fragmentHome;
 
     BottomNavigationView bottomNavigationView;
 
@@ -21,36 +27,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_bar);
+        fm.beginTransaction().add(R.id.fragment_display, fragmentFav, "2").hide(fragmentFav).commit();
+        fm.beginTransaction().add(R.id.fragment_display,fragmentHome, "1").commit();
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                //Al momento solo la home apre una nuova activity
-                Fragment selectedFragment = null;
-
                 switch (menuItem.getItemId()){
                     case R.id.action_home:
-                        selectedFragment = new Fragment_favorites();
-                        break;
+                        fm.beginTransaction().hide(active).show(fragmentHome).commit();
+                        active = fragmentHome;
+                        return true;
                     case R.id.action_search:
-                        selectedFragment = new Fragment_favorites();
-                        break;
+                        Toast.makeText(getApplicationContext(), "Favorite", Toast.LENGTH_SHORT).show();
+                        return true;
                     case R.id.action_add:
-                        selectedFragment = new Fragment_favorites();
-                        break;
+                        Toast.makeText(getApplicationContext(), "Favorite", Toast.LENGTH_SHORT).show();
+                        return true;
                     case R.id.action_favorites:
-                        selectedFragment = new Fragment_favorites();
-                        break;
+                        fm.beginTransaction().hide(active).show(fragmentFav).commit();
+                        active = fragmentFav;
+                        return true;
                     case R.id.action_profile:
-                        selectedFragment = new Fragment_favorites();
-                        break;
+                        Toast.makeText(getApplicationContext(), "Favorite", Toast.LENGTH_SHORT).show();
+                        return true;
                     default:
-                        selectedFragment = new Fragment_favorites();
+                        return false;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_display,selectedFragment).commit();
-
-                return true;
             }
         });
     }
