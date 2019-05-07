2f0fabc3f31375347e7f12afafa905b69dab99d5
TobiaC <tobia.cargnello@gmail.com>
2019-05-07 17:14:03 +0200

    created sign in and sign up activities

---
+++ app/src/main/java/com/example/foodmates/SignIn_Up.java
@@ -0,0 +1,13 @@
+package com.example.foodmates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in__up);
+    }
+}
